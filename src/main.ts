4a414c84038a4033de49f54d0bc4cab189841ade
kbvv <342768292@qq.com>
2021-03-20 23:02:10 +0800

    改了错误后

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('Nav', Nav);//全局引入Nav组件，其他地方直接使用，不需要再次import
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
