c0a54022622275c23ce009c534e35f9be9f2832b
kbvv <342768292@qq.com>
2021-03-21 20:32:41 +0800

    创建了Layout组件

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import Nav from '@/components/Nav.vue';
+import Layout from '@/components/Layout.vue';
 
 Vue.config.productionTip = false;
 Vue.component('Nav', Nav);//全局引入Nav组件，其他地方直接使用，不需要再次import
+Vue.component('Layout',Layout)
 
 new Vue({
   router,
